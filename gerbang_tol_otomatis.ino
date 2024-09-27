349b5b96bcaf1a1939234ccfb80ab29dcf4780d2
Ahmad Diffa <147916548+Diffajl@users.noreply.github.com>
2024-09-27 13:18:32 +0700

    Update gerbang_tol_otomatis.ino

--- gerbang_tol_otomatis.ino
+++ gerbang_tol_otomatis.ino
@@ -12,7 +12,7 @@ Servo servo;
 int angle = 10;
 long duration;
 int distance;
-bool buzzerPlayed = false; // Flag untuk melacak apakah buzzer sudah berbunyi
+bool buzzerPlayed = false; 
 
 void setup() {
   Serial.begin(9600);
@@ -22,29 +22,24 @@ void setup() {
   pinMode(ledRed, OUTPUT);
   pinMode(ledYellow, OUTPUT);
   pinMode(ledGreen, OUTPUT);
-  servo.attach(servoPin); // Hubungkan servo ke pin yang benar
+  servo.attach(servoPin);
 }
 
 void loop() {
-  // Mengirimkan sinyal trigger
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-  // Menghitung lama pantulan ultrasonik
   duration = pulseIn(echoPin, HIGH);
 
-  // Menghitung jarak dalam cm
   distance = duration * 0.034 / 2;
   Serial.print("Distance: ");
   Serial.println(distance);
   delay(100);
 
-  // Menggerakkan servo dan kontrol lampu/buzzer berdasarkan jarak
   if (distance < 10) {
-    // Kondisi jarak < 10 cm (Lampu hijau menyala)
     digitalWrite(ledRed, LOW);
     digitalWrite(ledYellow, HIGH);
     delay(500);
@@ -53,7 +48,6 @@ void loop() {
     servo.write(35);
 
     if (!buzzerPlayed) {
-      // Jika buzzer belum berbunyi, bunyikan buzzer sekali
       tone(buzzer, 1000, 100); // Durasi 100 ms
       buzzerPlayed = true; // Tandai bahwa buzzer sudah berbunyi
     }
@@ -65,7 +59,7 @@ void loop() {
   //   digitalWrite(ledRed, LOW);
   //   digitalWrite(ledGreen, LOW);
   //   buzzerPlayed = false; // Reset flag ketika keluar dari kondisi hijau
-  // } else {
+  } else {
     // Kondisi jarak lebih dari 50 cm (Lampu merah menyala)
     servo.write(130); // Menggerakkan servo ke 180 derajat
     digitalWrite(ledGreen, LOW);
